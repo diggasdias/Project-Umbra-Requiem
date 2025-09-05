c59fc60def63e5cb98b3bc335a4792f4bad041b1
diggasdias <diogoluizursulinodias@gmail.com>
2025-09-05 12:59:29 -0300

    ill returner

--- Project Umbra Requiem/Assets/Scripts/EnemyScripts/BeholderV1Config.cs
+++ Project Umbra Requiem/Assets/Scripts/EnemyScripts/BeholderV1Config.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
 
-public class BeholderV1Config : MonoBehaviour, IDamageable
+public class BeholderV1Config : MonoBehaviour
 {
     public int health = 3;
-
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     public void TakeDamage(int damage)
     {
         health -= damage;
         Debug.Log("teste funcionou"); 
         if (health <= 0)
             Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(CircleCollider2D circleCollider)
+    {
+        if (circleCollider.CompareTag("Scythe"))
+        {
+            TakeDamage(1);
+            animator.SetTrigger("Hit");
+        }
     }
 }
