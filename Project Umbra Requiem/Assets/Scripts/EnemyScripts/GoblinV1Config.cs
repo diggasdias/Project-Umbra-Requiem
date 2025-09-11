3029aa4a2b355bc1442e86a37040ebc465193a53
diggasdias <diogoluizursulinodias@gmail.com>
2025-09-11 14:25:26 -0300

    add enemies script health and dealth

---
+++ Project Umbra Requiem/Assets/Scripts/EnemyScripts/GoblinV1Config.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GoblinV1Config : MonoBehaviour
+{
+    public int health = 3;
+    private Animator animator;
+    void Start()
+    {
+        animator = FindAnyObjectByType<Animator>();
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            animator.SetTrigger("IsDead");
+            Destroy(gameObject, 0.9f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Scythe"))
+        {
+            animator.SetTrigger("Hit");
+            TakeDamage(1);
+        }
+    }
+}
