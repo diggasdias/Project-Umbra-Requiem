b2225994e12525ca7993ee7b9a181f06801f0765
Renan Prado <renanesct@gmail.com>
2025-09-12 15:48:12 -0300

    enemy AI recreated

--- Project Umbra Requiem/Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ Project Umbra Requiem/Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    Transform player;
+    public float moveSpeed = 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = FindAnyObjectByType<Player>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
     }
 }
