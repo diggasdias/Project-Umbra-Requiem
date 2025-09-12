e8bb9651752310286c704f9ba1049b4e2ab701e3
Renan Prado <renanesct@gmail.com>
2025-09-12 16:19:25 -0300

    created enemyspawner.cs

---
+++ Project Umbra Requiem/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Wave 
+    {
+        public string waveName;
+        public List<EnemyGroup> enemyGroups;
+        public int waveQuota;
+        public float spawnInterval;
+        public int spawnCount;
+    }
+
+    [System.Serializable]
+    public class EnemyGroup 
+    {
+        public string enemyName;
+        public int enemyCount;
+        public int spawnCount;
+        public GameObject enemyPrefab;
+    }
+
+
+    public List<Wave> waves;
+    public int currentWaveCount;
+
+    void Start()
+    {
+        
+    }
+
+
+    void CalculateWaveQuota()
+    {
+        int currentWaveQuota = 0;
+        foreach (var enemyGroup in waves[currentWaveCount].enemyGroups)
+        {
+            currentWaveQuota += enemyGroup.enemyCount;
+        }
+
+        waves[currentWaveCount].waveQuota = currentWaveQuota;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
