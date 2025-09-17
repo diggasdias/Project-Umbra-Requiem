2b42140c861ef214cd932a5ab9933584ef699110
Renan Prado <renanesct@gmail.com>
2025-09-17 12:58:43 -0300

    updated enemySpawner.cs

--- Project Umbra Requiem/Assets/Scripts/EnemyScripts/EnemyScriptableObject.cs
+++ Project Umbra Requiem/Assets/Scripts/EnemyScripts/EnemyScriptableObject.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using System.Runtime.CompilerServices;
 
 [CreateAssetMenu(fileName = "EnemyScriptableObject", menuName = "ScriptableObjects/Enemy")]
 public class EnemyScriptableObject : ScriptableObject
 {
-    public float moveSpeed;
-    public float maxHealth;
-    public float damage;
+    float moveSpeed;
+    public float MoveSpeed { get => moveSpeed; private set => moveSpeed = value; }
+    float maxHealth;
+    public float MaxHealth { get => maxHealth; private set => maxHealth = value; }
+    float damage;
+    public float Damage { get => damage; private set => damage = value; }
 }
