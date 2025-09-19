c651262e2ab9b6dd745cdc3b69b2138529399a89
Renan Prado <renanesct@gmail.com>
2025-09-19 08:17:58 -0300

    almost enemies damageable

--- Project Umbra Requiem/Assets/Scripts/Weapons/ProjectileWeaponBehaviour.cs
+++ Project Umbra Requiem/Assets/Scripts/Weapons/ProjectileWeaponBehaviour.cs
@@ -8,6 +8,20 @@ public class ProjectileWeaponBehaviour : MonoBehaviour
     public float destroyAfterSeconds;
     public WeaponScriptableObject weaponData;
 
+    //Current stats
+    protected float currentDamage;
+    protected float currentSpeed;
+    protected float currentCooldownDuration;
+    protected int currentPierce;
+
+    void Awake()
+    {
+        currentDamage = weaponData.Damage;
+        currentSpeed = weaponData.Speed;
+        currentCooldownDuration = weaponData.CooldownDuration;
+        currentPierce = weaponData.Pierce;
+    }
+
     protected virtual void Start()
     {
         Destroy(gameObject, destroyAfterSeconds);
@@ -61,6 +75,25 @@ public class ProjectileWeaponBehaviour : MonoBehaviour
             transform.localScale = scale;
         transform.rotation = Quaternion.Euler(rotation);
 
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            EnemyStats enemy = collision.GetComponent<EnemyStats>();
+            enemy.takeDamage(currentDamage);
+            ReducePierce();
+        }
+    }
+
+    void ReducePierce()
+    {
+        currentPierce--;
+        if (currentPierce <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
