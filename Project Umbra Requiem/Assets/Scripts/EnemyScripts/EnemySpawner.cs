97146f77b136b9e0d11181f69e901dd89db8f257
Renan Prado <renanesct@gmail.com>
2025-09-22 12:08:31 -0300

    initiatig the enemySpawner script

--- Project Umbra Requiem/Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Project Umbra Requiem/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -19,20 +19,35 @@ public class EnemySpawner : MonoBehaviour
     public class EnemyGroup 
     {
         public string enemyName;
-        public int enemyCount;
-        public int spawnCount;
+        public int enemyCount; //O número de inimigos a spawnar nessa wave
+        public int spawnCount; //O número de inimigos desse tipo q já spawnaram nessa wave
         public GameObject enemyPrefab;
     }
 
 
-    public List<Wave> waves;
+    public List<Wave> waves; //Lista de todas as waves do jogo
     public int currentWaveCount;
+
+    float spawnTimer;
+
+    Transform player;
 
     void Start()
     {
-        
+        CalculateWaveQuota();
+        player = FindAnyObjectByType<PlayerStats>().transform;
     }
 
+    void Update()
+    {
+        spawnTimer = Time.time;
+
+        if (spawnTimer >= waves[currentWaveCount].spawnInterval)
+        {
+            SpawnEnemies();
+            spawnTimer = 0f;
+        }
+    }
 
     void CalculateWaveQuota()
     {
@@ -57,14 +72,13 @@ public class EnemySpawner : MonoBehaviour
                 //Checa se o número minimo de inigos desse tipo já spawnou
                 if (enemyGroup.spawnCount < enemyGroup.enemyCount)
                 {
+                    Vector2 spawnPosition = new Vector2(player.transform.position.x + Random.Range(-10f, 10f), player.transform.position.y + Random.Range(-10f, 10f));
+                    Instantiate(enemyGroup.enemyPrefab, spawnPosition, Quaternion.identity);
 
+                    enemyGroup.spawnCount++;
+                    waves[currentWaveCount].spawnCount++;
                 }
             }
         }
-    }
-
-    void Update()
-    {
-        
     }
 }
