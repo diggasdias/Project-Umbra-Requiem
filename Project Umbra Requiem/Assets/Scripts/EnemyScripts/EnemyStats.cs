4590bb6ba89c9950e75389a31e487b07b4b0f7ee
Renan Prado <renanesct@gmail.com>
2025-09-23 15:53:45 -0300

    scene

--- Project Umbra Requiem/Assets/Scripts/EnemyScripts/EnemyStats.cs
+++ Project Umbra Requiem/Assets/Scripts/EnemyScripts/EnemyStats.cs
@@ -7,16 +7,29 @@ public class EnemyStats : MonoBehaviour
     //Status atuais
     [HideInInspector]
     public float currentMoveSpeed;
-    
+    [HideInInspector]
     public float currentHealth;
     [HideInInspector]
     public float currentDamage;
+
+    public float despawnDistance = 20f;
+    Transform player;
 
     void Awake()
     {
         currentMoveSpeed = enemyData.MoveSpeed;
         currentHealth = enemyData.MaxHealth;
         currentDamage = enemyData.Damage;
+    }
+
+    void Start()
+    {
+        player = FindAnyObjectByType<PlayerStats>().transform;   
+    }
+
+    void Update()
+    {
+        ReturnEnemy();   
     }
 
     public void TakeDamage(float dmg)
@@ -47,5 +60,11 @@ public class EnemyStats : MonoBehaviour
     {
         EnemySpawner es = FindAnyObjectByType<EnemySpawner>();
         es.OnEnemyKilled();
+    }
+
+    void ReturnEnemy()
+    {
+        EnemySpawner es = FindAnyObjectByType<EnemySpawner>();
+        transform.position = player.position + es.relativeSpawnPoints[Random.Range(0, es.relativeSpawnPoints.Count)].position;
     }
 }
