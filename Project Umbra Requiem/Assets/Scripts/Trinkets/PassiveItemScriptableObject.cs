ed7df19bdabad69551f17712886e3f0def071b77
Renan Prado <renanesct@gmail.com>
2025-09-24 12:59:06 -0300

    creating the passive itens

---
+++ Project Umbra Requiem/Assets/Scripts/Trinkets/PassiveItemScriptableObject.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PassiveItemScriptableObject : ScriptableObject
+{
+
+    float multiplier;
+    public float Multiplier { get => multiplier; private set => multiplier = value; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Start()
+    {
+
+    }
+
+}
