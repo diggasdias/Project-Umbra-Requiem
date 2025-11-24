5eabe59cac39e0fdb8072598ee01f5c086f20049
diggasdias <diogoluizursulinodias@gmail.com>
2025-11-24 16:20:21 -0300

    Unfinished lvl up

--- Project Umbra Requiem/Assets/Scripts/GameManager.cs
+++ Project Umbra Requiem/Assets/Scripts/GameManager.cs
@@ -81,7 +81,7 @@ public class GameManager : MonoBehaviour
                 {
                     choosingUpgrade = true;
                     Time.timeScale = 0f;
-                    Debug.Log("upgrades shown");
+                    //Debug.Log("upgrades shown");
                     levelUpScreen.SetActive(true);
                 }
                 break;
